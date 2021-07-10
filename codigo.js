a227f2d435516480b6986310e330a6d832b6cc77
Matías <barmat520@gmail.com>
2021-07-10 00:58:28 -0300

    first commit

---
+++ codigo.js
@@ -0,0 +1,13 @@
+let grados = prompt("¿Cuántos grados hace?")
+
+if (grados >= 30) {
+	alert("Que calor")
+}
+
+else if (grados <= 15) {
+	alert("Que frío")
+}
+
+else {
+	alert("Que bonito día hace")
+}
